b41179c88f894ac5242c44332ecbf38819a93a54
Aman Mittal <amandeepmittal@live.com>
2020-05-07 20:58:47 +0530

    initial commit

---
+++ styles1/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title]}>Hello world, from React Native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
