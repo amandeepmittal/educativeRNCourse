d0eb1e73c3ddd781483c2c1f77d7098b87bc0bdb
Aman Mittal <amandeepmittal@live.com>
2020-05-18 16:10:30 +0530

    initial commit

---
+++ flexbox/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'black',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        <Text style={{color: 'white', fontSize: 20}}>First Child</Text>
+        <Text style={{color: 'white', fontSize: 20}}>33%</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'gray',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        <Text style={{color: 'white', fontSize: 20}}>Second Child</Text>
+        <Text style={{color: 'white', fontSize: 20}}>33%</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'white',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+        <Text style={{color: 'black', fontSize: 20}}>Third Child</Text>
+        <Text style={{color: 'black', fontSize: 20}}>33%</Text>
+      </View>
+    </>
+  );
+}
